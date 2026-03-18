52ad6741349f95a5aaaa38855630c192531cc2aa
smsmorsalin <smsmorsalin1@gmail.com>
2026-03-19 00:03:44 +0600

    create form for notes_app

---
+++ notes_app/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen w-screen text-white'>
+      <h1 className='text-3xl font-bold text-center pt-10 w-full pb-10'>Notes App</h1>
+
+      <div className='p-10 border-2 lg:h-3/5 lg:w-2/5 rounded-2xl'>
+        <form>
+          <input
+          className='border-2 p-3 mt-5 w-full rounded-xl' 
+          type="text" 
+          placeholder='Note title' /> <br/>
+
+          <textarea
+          className='border-2 p-5 mt-5 w-full rounded-xl' 
+          placeholder='Note content' 
+          rows={10} /> <br/>
+
+          <button className='bg-blue-500 text-white px-4 py-2 mt-5 hover:bg-blue-600 rounded-xl'>Add Note</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
